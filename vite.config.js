c3d5a04369a40b4eac7d4f7c0c425ad6af979476
ekaterinakur <ekaterina1.kur@gmail.com>
2024-10-28 16:44:10 +0200

    Vercel dynamic routes fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: './',
+    base: '/',
     define: {
       'process.env': env,
     },
